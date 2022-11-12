1f067fc596a10e11402aad294f83d997dff2f04a
Victor Silva <victorguidasilva@gmail.com>
2022-11-12 15:37:17 -0300

    3.8*: Phonebook backend step8

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ const express = require('express')
 const app = express()
 const morgan = require('morgan')
 app.use(express.json())
-app.use(morgan('tiny'))
+
+morgan.token('body', (req) => JSON.stringify(req.body))
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 let persons = [
     { 
